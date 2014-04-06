c9670f6c0823d3e88b3aa1d03d7fa4605e167537
Toby <tlornewr@gmail.com>
2014-04-05 23:47:13 -0400

    gemfile and databases

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source :rubygems
+
+gem "sinatra"
+gem "sinatra-activerecord"
+gem "sqlite3"
+gem "rake"
--- site.rb
+++ site.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
+require "sinatra/activerecord"
+
+set :database, "sqlite3:///db.sqlite3"
 
 get '/' do
     erb :index
 end
+
