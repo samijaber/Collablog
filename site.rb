10d72a881e12ae9afc805978041811c2752f4950
Toby <tlornewr@gmail.com>
2014-04-06 00:07:03 -0400

    twitter authentication

--- site.rb
+++ site.rb
@@ -1,9 +1,35 @@
 require 'sinatra'
 require "sinatra/activerecord"
 
+#CONFIG
 set :database, "sqlite3:///db/db.sqlite3"
+configure do
+    enable :sessions
 
-get '/' do
-    erb :index
+    use OmniAuth::Builder do
+        provider :twitter, ENV["CONSUMER_KEY"], ENV["CONSUMER_SECRET"]
+    end
+end
+#HELPERS
+helpers do
+    def current_user
+        !session[:uid].nil?
+    end
+end
+#BEFORE
+before do
+    pass if request.path_info =~ /^\/auth\//
+    redirect to("/auth/twitter") unless current_user
+end
+#ROUTES
+get "/auth/twitter/callback" do
+    session[:uid] = env["omniauth.auth"]["uid"]
+    redirect to("/")
+end
+get "/auth/failure" do
+    "AUTH ERROR"
+end
+get "/" do
+    "AUTH WORKS"
 end
 
