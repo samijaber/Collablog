086af315cac22a292f081f2d4eb220aa41ba0ef5
Sami Jaber <jabersami@gmail.com>
2014-06-12 23:52:42 -0700

    The Scala revolution begins

---
+++ dogebarks/src/main/scala/com/dogebarks/app/DogeServlet.scala
@@ -0,0 +1,17 @@
+package com.dogebarks.app
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class DogeServlet extends DogebarksStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+  
+}
