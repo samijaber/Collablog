d4f8aacc68cea77f6a3999e99f4912cffc48a3df
Sami Jaber <jabersami@gmail.com>
2014-06-14 23:30:36 -0700

    setup layout

--- dogebarks/src/main/scala/com/dogebarks/app/DogeServlet.scala
+++ dogebarks/src/main/scala/com/dogebarks/app/DogeServlet.scala
@@ -5,15 +5,19 @@ import scalate.ScalateSupport
 
 class DogeServlet extends DogebarksStack with ScalateSupport {
 
-  get("/") {
-  	contentType="text/html"
+get("/") {
+	contentType="text/html"
+	ssp("/index")
+	// layoutTemplate("/WEB-INF/templates/views/index.ssp")
+}
 
-  	layoutTemplate("/WEB-INF/templates/views/index.ssp")
-  }
-
+get("/auth") {
+	"twitter auth to do here"
+	//twitter callback redirect
+}
 
 object Helpers {
-  def current_user() = true;
+	def current_user() = true;
 }
   
 }
