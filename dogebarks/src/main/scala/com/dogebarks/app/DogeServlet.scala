93249490b31155504a1a61437fa2e73ce168d702
Sami Jaber <jabersami@gmail.com>
2014-06-19 22:46:03 -0700

    accessToken stored, user timeline retrievable

--- dogebarks/src/main/scala/com/dogebarks/app/DogeServlet.scala
+++ dogebarks/src/main/scala/com/dogebarks/app/DogeServlet.scala
@@ -16,6 +16,8 @@ object Helpers {
 	def current_user() = true;
 }
 
+var accTkn: Token = _;
+
 get("/") {
 	contentType="text/html"
 	ssp("/index")
@@ -29,8 +31,15 @@ get("/auth") {
 
 get("/auth/callback") {
 	val requestToken: Token = new Token(params("oauth_token"), Secret.apiSecret)
-	val accTkn = TwitterOAuth.getAccessToken(requestToken, params("oauth_verifier"))
+	accTkn = TwitterOAuth.getAccessToken(requestToken, params("oauth_verifier"))
 	redirect("/")
+}
+
+get("/blog") {
+	val req = new OAuthRequest(Verb.GET, "https://api.twitter.com/1.1/statuses/user_timeline.json")
+	TwitterOAuth.service.signRequest(accTkn, req)
+	val resp = req.send()
+	resp.getBody()
 }
 
 }
