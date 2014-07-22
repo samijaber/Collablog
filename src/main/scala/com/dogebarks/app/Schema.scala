963e30f91abb413056c09f79b0eca802dd654d16
Sami Jaber <jabersami@gmail.com>
2014-07-22 02:43:52 -0700

    added time columns

--- src/main/scala/com/dogebarks/app/Schema.scala
+++ src/main/scala/com/dogebarks/app/Schema.scala
@@ -1,33 +1,34 @@
 package com.dogebarks.app
 
 import scala.slick.driver.H2Driver.simple._
-import java.sql._
 
 object Schema {
-	class Blogs(tag: Tag) extends Table[(String, String, String)](tag, "BLOGS") {
-		def hashtag = column[String]("HASHTAG", O.PrimaryKey)
-		def owner 	= column[String]("OWNER")
-		def title		= column[String]("TITLE")
-		def * 			= (hashtag, owner, title)
+	class Blogs(tag: Tag) extends Table[(String, String, String, String)](tag, "BLOGS") {
+		def hashtag 	= column[String]("HASHTAG", O.PrimaryKey)
+		def owner 		= column[String]("OWNER")
+		def title			= column[String]("TITLE")
+		def createdAt = column[String]("CREATEDAT")
+		def * 				= (hashtag, owner, title, createdAt)
 	}
 	val blogs = TableQuery[Blogs]
 
 
-	class Contributors(tag: Tag) extends Table[(String, String)](tag, "CONTRIBUTORS") {
+	class Contributors(tag: Tag) extends Table[(String, String, String)](tag, "CONTRIBUTORS") {
 		def userId 		= column[String]("USERID")
 		def hashtag 	= column[String]("HASHTAG")
-		def * 				= (userId, hashtag)
+		def addedAt 	= column[String]("ADDEDAT")
+		def * 				= (userId, hashtag, addedAt)
 
 		// def story 		= foreignKey("STORY", hashtag, blogs)(_.hashtag)
 	}
 	val contributors = TableQuery[Contributors]
 
 
-	class Tweets(tag: Tag) extends Table[(String, String, String, Timestamp)](tag, "TWEETS") {
+	class Tweets(tag: Tag) extends Table[(String, String, String, String)](tag, "TWEETS") {
 		def url 			= column[String]("URL", O.PrimaryKey)
 		def text 			= column[String]("TEXT")
 		def userId 		= column[String]("USERID")
-		def createdAt = column[Timestamp]("CREATEDAT")
+		def createdAt = column[String]("CREATEDAT")
 		def * 				= (url, text, userId, createdAt)
 
 		// def writer = ("WRITER", userId, contributors)(_.userId)
